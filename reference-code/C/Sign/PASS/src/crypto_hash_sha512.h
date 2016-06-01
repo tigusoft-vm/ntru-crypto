5bdf2be3afa2861c16b2c8e608dce8ff32e7f30b
Rafal Crypto Tigusoft <rafal2@tigusoft.pl>
2016-06-01 16:54:11 +0000

    add ifndef to avoid sha512 duplicate vs sodium

--- reference-code/C/Sign/PASS/src/crypto_hash_sha512.h
+++ reference-code/C/Sign/PASS/src/crypto_hash_sha512.h
@@ -1,10 +1,15 @@
 #ifndef HASH_H
 #define HASH_H
 
+#ifndef crypto_hash_sha512_BYTES
+// we already have this things e.g. from libsodium (from system)
+
 int crypto_hashblocks_sha512(unsigned char *statebytes,const unsigned char *in,unsigned long long inlen);
 
 int crypto_hash_sha512(unsigned char *out,const unsigned char *in,unsigned long long inlen);
 
 #define crypto_hash_sha512_BYTES 64
+
+#endif
 
 #endif
